48ea9a7bf1e4eb6170b2e423b0e94dfacfaec0ad
UmamaMasroor <umamafatima319@gmail.com>
2024-05-28 00:30:16 +0500

    Day 2 😊

---
+++ Day_02/main.js
@@ -0,0 +1,11 @@
+// Question 1 Famous Quote
+var author = 'Allama Iqbal';
+console.log("".concat(author, " once said, \"People who have no hold over their process of thinking ara likely to be ruined by liberty of thought.\""));
+// Question 2 Famous Quote 2
+var famous_person = 'Allama Iqbal';
+var famous_quote = '"People who have no hold over their process of thinking ara likely to be ruined by liberty of thought."';
+console.log("".concat(author, " once said, ").concat(famous_quote));
+// Question 3 Stripping Names
+var name1 = 'Fatimah';
+console.log("With whitespaces \n \t ".concat(name1, " \n \t"));
+console.log("Shows the name without whitespace : ".concat(name1.trim()));
---
+++ Day_02/main.ts
@@ -0,0 +1,17 @@
+// Question 1 Famous Quote
+
+let author = 'Allama Iqbal';
+console.log(`${author} once said, "People who have no hold over their process of thinking ara likely to be ruined by liberty of thought."` );
+
+
+// Question 2 Famous Quote 2
+let famous_person = 'Allama Iqbal';
+let famous_quote = '"People who have no hold over their process of thinking ara likely to be ruined by liberty of thought."'
+console.log(`${author} once said, ${famous_quote}` );
+
+
+// Question 3 Stripping Names
+let name1 = 'Fatimah';
+console.log(`With whitespaces \n \t ${name1} \n \t`);
+console.log(`Shows the name without whitespace : ${name1.trim()}`);
+
